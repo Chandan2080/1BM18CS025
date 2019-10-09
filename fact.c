43ea74bcbc293c866fe0ef7c01b04062b18245f6
Chandan <36081499+Chandan2080@users.noreply.github.com>
2019-10-09 10:43:51 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,16 @@
+//Factorial using recursion:
+#include<conio.h>
+#include<stdio.h>
+long fact(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n*fact((n-1));
+}
+int main(){
+    int n;
+    printf("Enter the number:    ");
+    scanf("%d",&n);
+    printf("The factorial of %d is %ld",n,fact(n));
+    getch();
+}
