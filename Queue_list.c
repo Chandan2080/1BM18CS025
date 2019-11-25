aecfdd4b57f66d378dd868f87425b1c67fe633e0
Chandan <36081499+Chandan2080@users.noreply.github.com>
2019-11-25 20:51:37 +0530

    Update Queue_list.c

--- Queue_list.c
+++ Queue_list.c
@@ -1,95 +1,90 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define size 5
-struct node{
+struct node
+{
 	int data;
 	struct node *next;
 };
 typedef struct node* NODE;
 NODE getnode(){
 	NODE p;
-	p=(NODE)malloc(sizeof(struct node));
-	if(p!=NULL)
+	p = (NODE)malloc(sizeof(struct node));
+	if(p != NULL)
 		return p;
 	else{
 		printf("No memory allocation\n");
 		exit(0);
 	}
 }
-NODE insert(NODE head,int item){
-	NODE p, q=head;
+NODE insert(NODE head, int item){
+	NODE p,q = head;
 	p = getnode();
 	p->data = item;
 	if(head == NULL){
 		p->next = NULL;
-		head = p;
+	 	head = p;
 		return head;
 	}
 	else{
-		q=head;
-		while(q->next!=NULL){
-			q=q->next;
-		}
-		q->next=p;
-		return head;
-	}
+	    q = head;
+	    while(q->next!=NULL)
+	    	q = q->next;
+	    q->next = p;
+	    return head;
+	   }
 }
-NODE delete(NODE head)
-{
+NODE delete(NODE head){
 	NODE p;
 	int item;
 	if(head==NULL){
-		printf("Undreflow\n");
+		printf("Queue Empty\n");
 		return head;
 	}
 	else{
-		item=head->data;
-		p=head;
-		head=head->next;
+		item = head->data;
+		p = head;
+		head = head->next;
 		free(p);
 		printf("Deleted element is %d\n",item);
 		return head;
 	}
 }
-void display(NODE head)
-{
+void display(NODE head){
 	NODE p;
-	if(head == NULL)
-	{
+	if(head==NULL){
 		printf("Empty Queue\n");
+		return;
 	}
-	else{
-	    p=head;
-	    printf("Elements in Queue are\n");
-	    while(p!=NULL)
-	    {
-    		printf("%d\n",p->data);
-    		p=p->next;
-	    }
+	p = head;
+	printf("Elements in Queue are:\t");
+	while(p != NULL)
+	{
+	    printf("%d\t",p->data);
+	    p = p->next;
 	}
 }
-int main()
-{
-	NODE head=NULL;
-	int ch,ele;
+int main(){
+	NODE head = NULL;
+	int ch, ele;
 	do{
 	    printf("\n\n1 : Insert an element");
 	    printf("\n2 : Delete an element");
-	    printf("\n3 : Display all the elements");
+	    printf("\n3 : Display the elements");
 	    printf("\n4 : EXIT");
 	    printf("\nEnter your choise:\t");
 	    scanf("%d",&ch);
 		switch(ch){
-			case 1: printf("Enter element to be inserted\n");
+			case 1: printf("Enter element to be inserted:\t");
 				    scanf("%d",&ele);
-			    	head=insert(head,ele);
+				    head=insert(head,ele);
 				    break;
 			case 2: head=delete(head);
 				    break;
 			case 3: display(head);
 				    break;
-			case 4: exit(0);
-			default:printf("\nInvalid choise!!");
+			case 4: break;
+			default:printf("Invalid\n");
 		}
 	}while(ch!=4);
 	return 0;
